960132cea04fddceffd6b767042bf73a903c8009
George Yeh <124641066+ccyehintx@users.noreply.github.com>
2023-05-19 23:29:34 -0500

    Add files via upload

--- multifrac_time.py
+++ multifrac_time.py
@@ -36,7 +36,7 @@ def hs_con(fq_ls, s_ls):
     for i in range(len(s_ls)):
         fq_ln.append(np.log(fq_ls[i]))
         s_ln.append(np.log(s_ls[i]))
-    gr, con = np.polyfit(s_ln, fq_ln, 2)
+    gr, con = np.polyfit(s_ln, fq_ln, 1)
     return gr
 
 def tau_ls(hs, q):
@@ -54,11 +54,11 @@ def alpha_fd(x, y):
         # grab all q except the first one
     return alpha_ls
 
-def extract_file(file, si, fi), ex_id:
+def extract_file(file, si, fi, ex_id):
     # 152, 1152, 2
     sf = fi - si
     mylines = []
-    with open('log.lammps', 'rt') as myfile:
+    with open(file, 'rt') as myfile:
         for myline in myfile:
             mylines.append(myline)
     x = range(0, sf)
@@ -66,8 +66,8 @@ def extract_file(file, si, fi), ex_id:
     for i in range(si, fi):
         iv = mylines[i].split()[ex_id]
         ii = round(eval(iv), 4)
-        real_num.append(ii)#mylines[i])
-    return mylines
+        real_num.append(ii)
+    return real_num
 
 def profile_ls(real_num):
     avg = sum(real_num)/len(real_num)
@@ -79,14 +79,34 @@ def profile_ls(real_num):
         cur_sum = iele
     return profile
 
-q = 3.5
+raw_data = extract_file('log.lammps', 152, 1152, 2)
+profile = profile_ls(raw_data)
 
+q_ls = list(np.arange(-2, 4, 0.3))
 s_ls = [5, 10, 20, 40, 50, 100, 200, 250]
-cnt_fq = []
-for s in s_ls:
-    f2 = f_2sq(s, profile)
-    fq = f_q(f2, s, q)
-    print(fq)
-    cnt_fq.append(fq)
-#print(cnt_fq)
-print(s_ls)
+all_tau = []
+for q in q_ls:
+    cnt_fq = []
+    for s in s_ls:
+        f2 = f_2sq(s, profile)
+        fq = f_q(f2, s, q)
+        cnt_fq.append(fq)
+    hs = hs_con(cnt_fq, s_ls)
+    tau = tau_ls(hs, q)
+    all_tau.append(tau)
+al_ls = alpha_fd(q_ls, all_tau)
+
+f_ls = []
+for a in range(len(al_ls)):
+    aa = a + 1
+    ff = al_ls[a]*q_ls[aa] - all_tau[aa]
+    f_ls.append(ff)
+    #print(ff, al_ls[a], q_ls[aa])
+
+plt.plot(al_ls, f_ls, label='Original')
+plt.xlabel('Singularity Strength')
+plt.ylabel('Singularity Spectrum')
+plt.title('Multifractal Spectrum')
+#plt.legend()
+plt.show()
+#plt.savefig('q1.png')
